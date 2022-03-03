9d8df92b1be22977ebd77a3b467106ffea326491
Tigran <tig.vardanyan.001@gmail.com>
2022-03-04 01:21:18 +0400

    grouping

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const banner_colser = document.querySelector("#banner_colser");
-const wrapper = document.querySelector(".wrapper");
-
-
-banner_colser.addEventListener("click", function() {
-    wrapper.remove();
-});
